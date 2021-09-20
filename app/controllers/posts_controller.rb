2faf1e5e2226c57184100a5feed82a7f4a651643
dotnotation <dotinmotionproductions@gmail.com>
2021-09-20 14:37:43 -0400

    commit tree 6ae9df26c88d6dbf87f5cf8358245ed4afe1b1be

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,14 @@ class PostsController < ApplicationController
   end
 
   def new
-    @post = Post.new
+    if params[:author_id] && !Author.exists?(params[:author_id])
+      redirect_to authors_path, alert: "Author not found."
+    else
+      @post = Post.new(author_id: params[:author_id])
+    end
+    # now that post is a nested resource, it now has access to author_id 
+    # we want our new post to be assigned to an author
+    # make sure you add author_id to strong params
   end
 
   def create
@@ -33,12 +40,24 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
+    # want to make sure that the author_id is valid and the post matches the author
+    if params[:author_id]
+      author = Author.find_by(id: params[:author_id])
+      if author.nil?
+        redirect_to authors_path, alert: "Author not found."
+      else
+        @post = author.posts.find_by(id: params[:id])
+        redirect_to author_posts_path(author), alert: "Post not found." if @post.nil?
+      end
+    else
+      @post = Post.find(params[:id])
+    end
   end
 
   private
 
   def post_params
-    params.require(:post).permit(:title, :description)
+    params.require(:post).permit(:title, :description, :author_id)
+    # allows for mass assignment
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :authors, only: [:show, :index] do
+    resources :posts, only: [:show, :index, :new, :edit]
+    # gives access to /authors/:author_id/posts/new and new_author_post_path helper
+  end
+  resources :posts
 end
